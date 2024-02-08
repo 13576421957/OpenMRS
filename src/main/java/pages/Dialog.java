b6b8da5059d9c3919422641834d3c0cdcb567137
assiya <assiyazee@gmail.com>
2024-02-08 16:51:13 -0500

    3comit

---
+++ src/main/java/pages/Dialog.java
@@ -0,0 +1,27 @@
+package pages;
+import org.apache.xmlbeans.impl.xb.xsdschema.FieldDocument;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import utilities.DriverClass;
+
+import javax.swing.text.html.CSS;
+
+public class Dialog {
+    public Dialog() {
+
+    }
+    @FindBy(css=("#username"))
+    public WebElement username;
+
+    @FindBy(css =("#password"))
+    public WebElement password;
+
+    @FindBy(css = ("#loginButton"))
+    public WebElement loginButton;
+
+
+
+}
+
+
---
+++ src/main/java/stepdefinitions/_01_loginSteps.java
@@ -0,0 +1,39 @@
+package stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_loginSteps {
+    @Given("Navigate to OpenMRS Website")
+    public void navigate_to_open_mrs_website() {
+
+    }
+    @Given("Enter valid username into the {string} field")
+    public void enter_valid_username_into_the_field(String string) {
+
+    }
+    @Given("Enter valid password in to the {string} field")
+    public void enter_valid_password_in_to_the_field(String string) {
+
+    }
+    @When("Click on the login button")
+    public void click_on_the_login_button() {
+
+    }
+    @Then("Should not able to login")
+    public void should_not_able_to_login() {
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
